7c78ca58c4326af271eb75444a579fd95594f929
Kao Saelor <saelor.kao@gmail.com>
2024-11-09 11:52:10 -0800

    start on footer

---
+++ fin-edu-games/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ fin-edu-games/src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+      <footer>
+        <div className="footer__copyright">
+          <small>&copy; 2025 Kao Saelor. All rights reserved.</small>
+        </div>
+      </footer>
+  
+    )
+  }
+  
+  export default Footer
