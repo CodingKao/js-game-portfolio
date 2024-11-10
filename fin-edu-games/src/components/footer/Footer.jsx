0ac9da0a707406176764c7913132ed041bd16be8
Kao Saelor <saelor.kao@gmail.com>
2024-11-09 16:31:18 -0800

    proof of life footer

---
+++ fin-edu-games/src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './footer.css';
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer__copyright">
+        <small>&copy; 2023 Kao Saelor. All rights reserved.</small>
+      </div>
+    </footer>
+
+  )
+}
+
+export default Footer
