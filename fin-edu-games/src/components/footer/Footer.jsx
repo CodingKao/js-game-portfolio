e3631145df03ca2515692f41c994ecbe0005415e
Kao Saelor <saelor.kao@gmail.com>
2024-11-10 16:05:49 -0800

    work on adding social to footer

--- fin-edu-games/src/components/footer/Footer.jsx
+++ fin-edu-games/src/components/footer/Footer.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+// Footer.jsx
+import React from 'react';
 import './footer.css';
-
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer__copyright">
-        <small>&copy; 2025 Kao Saelor. All rights reserved.</small>
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="copyright">© 2024 SmartFinance Playground. All rights reserved.</p>
+        <div className="social-media">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="facebook-icon.png" alt="Facebook" />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="twitter-icon.png" alt="Twitter" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="instagram-icon.png" alt="Instagram" />
+          </a>
+        </div>
       </div>
     </footer>
+  );
+};
 
-  )
-}
-
-export default Footer
+export default Footer;
